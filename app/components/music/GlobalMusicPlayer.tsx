bf7748cb074988998b553ea40dff1e3b966af8f5
jose <jose@MacBook-Air-de-jose.local>
2026-08-13 14:11:50 +0200

    Desactivar Picture in Picture en recomendaciones

--- app/components/music/GlobalMusicPlayer.tsx
+++ app/components/music/GlobalMusicPlayer.tsx
@@ -141,7 +141,7 @@ function YouTubeRecommendationPlayer({
       className={styles.youtubeEmbed}
       src={`https://www.youtube-nocookie.com/embed/${videoId}?rel=0&autoplay=1&enablejsapi=1`}
       title={title}
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; web-share"
       allowFullScreen
     />
   );
