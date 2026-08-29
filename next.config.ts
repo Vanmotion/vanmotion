62d2254d36ed2b74b3eb24d259653ec58b7d2b2a
José <tu-email-de-github>
2026-08-29 15:06:41 +0200

    Activa Content Security Policy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const contentSecurityPolicyReportOnly = [
+const contentSecurityPolicy = [
   "default-src 'self'",
   "base-uri 'self'",
   "object-src 'none'",
@@ -59,8 +59,8 @@ serverActions: {
               "camera=(), microphone=(), geolocation=()",
           },
           {
-            key: "Content-Security-Policy-Report-Only",
-            value: contentSecurityPolicyReportOnly,
+            key: "Content-Security-Policy",
+            value: contentSecurityPolicy,
           },
         ],
       },
